4d91f8e97d6a9bc938e17f937390545fe09c017d
Grimshaw <artturmoreira@gmail.com>
2013-03-27 18:45:29 +0000

    Removed useless Sources

--- Source/ParticlePointRenderer.cpp
+++
@@ -1,9 +0,0 @@
-/*#include "Nephilim/ParticlePointRenderer.h"
-
-PARABOLA_NAMESPACE_BEGIN
-
-SPK::Ref<ParticlePointRenderer> ParticlePointRenderer::create(float screenSize){
-	return SPK::Ref<ParticlePointRenderer>();
-};
-
-PARABOLA_NAMESPACE_END*/
--- Source/ParticleSystemWrapper.cpp
+++
@@ -1,9 +0,0 @@
-#ifndef MINIMAL_BUILD
-
-#include "Nephilim/ParticleSystem.h"
-
-PARABOLA_NAMESPACE_BEGIN
-	
-PARABOLA_NAMESPACE_END
-
-#endif
