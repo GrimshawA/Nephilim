30f6a1691564ba8146da3e7e8e631933c2cf250a
Grimshaw <artturmoreira@gmail.com>
2014-01-14 02:20:35 +0000

    New stuff?

---
+++ include/Nephilim/NinePatchShape.h
@@ -0,0 +1,16 @@
+#ifndef NinePatchShape_h__
+#define NinePatchShape_h__
+
+#include "Platform.h"
+#include "VertexData.h"
+
+NEPHILIM_NS_BEGIN
+
+class NEPHILIM_API NinePatchShape
+{
+public:
+	VertexData geometry;
+};
+
+NEPHILIM_NS_END
+#endif // NinePatchShape_h__
